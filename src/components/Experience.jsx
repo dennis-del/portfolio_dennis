42454c2e0cc0548e364c7ce90e4d8612175ead49
dennis-del <dennisjames991@gmail.com>
2025-02-19 16:41:54 +0530

    changed skills

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -22,9 +22,7 @@ function Experience() {
                         <h3 className='mb-2 font-semibold'>Software Developer - <span className='text-sm text-stone-500'>Clovers net solution</span></h3>
                         <p className='mb-4 text-stone-400'>
 I’ve been working on a professional educational app for a client named Skill Clovers. The app is built using React Native and Expo CLI, with a focus on delivering domain-specific courses. I’ve implemented features like domain and goal-based course categorization, a module system for learning materials, and integrated Razorpay for secure payments. The app offers tiered payment options, granting different levels of access to course videos. I’ve also designed a scalable backend with efficient schemas for domains, courses, and payments, ensuring the app can grow with the client’s needs.</p>
-                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Razorpay</span>
                         <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>React Native</span>
-                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Expo CLI</span>
                         <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>MongoDB</span>
                         <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Node.js</span>
                         <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Express</span>
@@ -39,11 +37,10 @@ I’ve been working on a professional educational app for a client named Skill C
                         <p className='mb-4 text-stone-400'>Developed and maintained web applications using Mearn stack.Designed and implemented RESTful APIs for
                             data communication.Collaborated with cross-functional teams to deliver high quality software products on schedule.</p>
                         <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Javascript</span>
-                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Angular</span>
-                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Node.js</span>
-                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>React</span>
-                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>MongoDB</span>
-                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Express</span>
+                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Bootstrap</span>
+                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Vercel</span>
+                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Github</span>
+                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Render</span>
                     </motion.div>
                 </div>
                 <div className='mb-8 flex flex-wrap lg:justify-center'>
