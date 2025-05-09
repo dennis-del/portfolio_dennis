cbbe7c40cddf6c9f5fee344d12a1976e995ef586
dennis-del <dennisjames991@gmail.com>
2025-05-09 15:51:43 +0530

    new company added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -14,6 +14,21 @@ function Experience() {
             </Helmet>
             <motion.h2 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} transition={{ duration: 0.5 }} className='my-20 text-center text-4xl'>Experience</motion.h2>
             <div>
+            <div className='mb-8 flex flex-wrap lg:justify-center'>
+                    <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className='w-full lg:w-1/4'>
+                        <p className='mb-2 text-sm text-stone-400'>03/2025 - Present</p>
+                    </motion.div>
+                    <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1 }} className='w-full max-w-xl lg:w-3/4'>
+                        <h3 className='mb-2 font-semibold'>Jr Mern Stack Developer - <span className='text-sm text-stone-500'>Wisbato</span></h3>
+                        <p className='mb-4 text-stone-400'>
+                        I contributed as a Junior Software Developer to CRM-based trading web applications, supporting local client needs. My responsibilities included frontend and backend development using the MERN stack, implementing user interfaces, integrating APIs, and handling data management tasks.
+
+During my time here, I strengthened my skills in TypeScript, React with Vite, and backend integration, and gained practical experience in admin panel design, permission-based user management, and project structuring. I also learned to collaborate effectively in a fast-paced team environment, ensuring project scalability and maintainability.</p>
+                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Next Js</span>
+                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Mern</span>
+                        <span className='mr-2 mt-4 rounded bg-stone-700 px-2 py-1 text-sm font-medium text-stone-300'>Typescript</span>
+                    </motion.div>
+                </div>
                 <div className='mb-8 flex flex-wrap lg:justify-center'>
                     <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className='w-full lg:w-1/4'>
                         <p className='mb-2 text-sm text-stone-400'>11/2024 - 01/2025</p>
