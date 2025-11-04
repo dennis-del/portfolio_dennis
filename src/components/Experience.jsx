24e16ab608871b1702f91402193833366266cce2
dennis-del <dennisjames991@gmail.com>
2025-11-04 11:13:31 +0530

    experience date changed

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -31,7 +31,7 @@ During my time here, I strengthened my skills in TypeScript, React with Vite, an
                 </div>
                 <div className='mb-8 flex flex-wrap lg:justify-center'>
                     <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1 }} className='w-full lg:w-1/4'>
-                        <p className='mb-2 text-sm text-stone-400'>11/2024 - 01/2025</p>
+                        <p className='mb-2 text-sm text-stone-400'>10/2024 - 01/2025</p>
                     </motion.div>
                     <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1 }} className='w-full max-w-xl lg:w-3/4'>
                         <h3 className='mb-2 font-semibold'>Software Developer - <span className='text-sm text-stone-500'>Clovers net solution</span></h3>
